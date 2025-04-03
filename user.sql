bd63a21b0ab4580e7839ef9315d3193560d1b09b
Richie Zhang <richiezhang@protonmail.com>
2025-04-03 10:31:40 -0700

    Make code style consistent

--- user.sql
+++ user.sql
@@ -3,7 +3,8 @@ use pacman_db;
 -- USER[UserID, name, email]
 
 CREATE TABLE `user` (
-    `user_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT NOT NULL,
     `name` VARCHAR(100) NOT NULL,
-    `email` VARCHAR(100) NOT NULL
+    `email` VARCHAR(100) NOT NULL,
+    CONSTRAINT usr_pk PRIMARY KEY (user_id),
 );
